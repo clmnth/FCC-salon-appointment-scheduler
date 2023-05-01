98eac2ac62e21abfa0e558927b8dfa4f851b8447
clmnt <huvigclement@gmail.com>
2023-05-01 19:41:32 +0200

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -1,1 +1,27 @@
+#! /bin/bash
 
+PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
+
+echo -e "\n~~~~~ Beauty Salon ~~~~~\n"
+echo -e "\nBook your next appointment today!"
+
+MAIN_MENU() {
+    SERVICES=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id")
+    echo "$SERVICES" | while read SERVICE_ID BAR NAME
+    do
+        echo "$SERVICE_ID) $NAME "
+    done
+
+    # service selected
+    read SERVICE_ID_SELECTED
+    SERVICE_EXIST=$($PSQL " SELECT service_id FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+
+    if [[ -z $SERVICE_EXIST ]]
+    then
+        # send to main menu
+        echo "I could not find that service. What would you like to book?"
+        MAIN_MENU
+    fi
+}
+
+MAIN_MENU
