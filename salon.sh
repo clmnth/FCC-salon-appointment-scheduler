fc32620987fc58b53aa0a433566b38d6a5b890ad
clmnt <huvigclement@gmail.com>
2023-05-02 12:23:16 +0200

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -5,23 +5,48 @@ PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
 echo -e "\n~~~~~ Beauty Salon ~~~~~\n"
 echo -e "\nBook your next appointment today!"
 
-MAIN_MENU() {
+
     SERVICES=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id")
-    echo "$SERVICES" | while read SERVICE_ID BAR NAME
+    echo "$SERVICES" | while read SERVICE_ID BAR SERVICE_NAME
     do
-        echo "$SERVICE_ID) $NAME "
+        echo "$SERVICE_ID) $SERVICE_NAME"
     done
 
-    # service selected
-    read SERVICE_ID_SELECTED
-    SERVICE_EXIST=$($PSQL " SELECT service_id FROM services WHERE service_id=$SERVICE_ID_SELECTED")
 
+    # service selected
+    read SERVICE_SELECTED_ID
+    SERVICE_EXIST=$($PSQL " SELECT service_id FROM services WHERE service_id=$SERVICE_SELECTED_ID")
+
+    # if service doesnt exist
     if [[ -z $SERVICE_EXIST ]]
     then
         # send to main menu
         echo "I could not find that service. What would you like to book?"
         MAIN_MENU
-    fi
-}
+    else
+    # get customer info
+        echo -e "\nWhat's your phone number?"
+        read CUSTOMER_PHONE
 
-MAIN_MENU
+        CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+
+        # if customer doesn't exist
+        if [[ -z $CUSTOMER_NAME ]]
+        then
+          # get new customer name
+          echo -e "\nI don't have a record for that phone number, what's your name?"
+          read CUSTOMER_NAME
+        
+
+         # insert new customer
+          INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')") 
+        fi
+
+        # get service selected name
+        SERVICE_SELECTED_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_SELECTED_ID ")
+
+        # get customer booking time
+        echo -e "\nWhat time would you like your $(echo $SERVICE_SELECTED_NAME | sed -r 's/^ *| *$//g'), $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')?"
+        read SERVICE_TIME
+
+    fi
